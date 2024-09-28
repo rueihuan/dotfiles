bc005f1c21289ea4ca2713fd36638e84f5b5994c
Ruei <23100072+rueihuan@users.noreply.github.com>
2024-09-27 23:08:36 -0700

    updated config of alacritty and fish

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     and test "$XDG_SESSION_TYPE" = "wayland"
 
+    set -x GTK_IM_MODULE fcitx
+
     # for monitor in $(hyprctl monitors | grep 'Monitor' | awk '{ print $2 }'); do
 
     # hyprctl hyprpaper preload "path/to/wallpaper"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,12 @@
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
+set -gx RUST_HOME "$HOME/.cargo/bin"
+set -gx PATH $PNPM_HOME $RUST_HOME $PATH
 set -Ux PAGER less
 set -Ux EDITOR vim
 set -Ux VISUAL vim
 
 switch (uname)
     case Linux
-        # set -x GTK_IM_MODULE fcitx
-        set -x QT_IM_MODULE fcitx
-        set -x XMODIFIERS @im=fcitx
     case Darwin
     case '*'
 end
