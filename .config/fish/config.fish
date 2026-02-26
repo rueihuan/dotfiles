7b71f1e68fe389a59bb6e73f34d2a2022a56f43a
Rui Chen <ruihuchen@paypal.com>
2026-02-26 12:58:18 -0800

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,17 @@
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-set -gx RUST_HOME "$HOME/.cargo/bin"
-set -gx PATH $PNPM_HOME $RUST_HOME $PATH
 set -Ux PAGER less
 set -Ux EDITOR vim
 set -Ux VISUAL vim
 
+set -g fish_color_command brgreen
+set -g fish_color_error brmagenta
+
 fish_vi_key_bindings
+
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+fish_add_path $PNPM_HOME
+
+set -gx RUST_HOME "$HOME/.cargo/bin"
+fish_add_path $RUST_HOME
 
 if command -v fnm > /dev/null
     fnm env --use-on-cd --shell fish | source
@@ -18,3 +24,4 @@ switch (uname)
     case Darwin
     case '*'
 end
+
